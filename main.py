703e5945383edf6aba322edd97424c059163e32e
Joonas Erkheikki <erkheikki@hotmail.com>
2020-05-27 01:12:41 +0300

    ledtest

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import time
 import modules.test as test
 from modules.led import LedControl
 
+RED = (255, 0, 0, 0)
+GREEN = (0, 255, 0, 0)
+BLUE = (0, 0, 255, 0)
+
 
 if __name__ == "__main__":
     while True:
@@ -12,6 +16,13 @@ if __name__ == "__main__":
         # Handle change
         print("test")
         time.sleep(1)
+        LedControl.change_color(RED)
+        time.sleep(1)
+        LedControl.change_color(GREEN)
+        time.sleep(1)
+        LedControl.change_color(BLUE)
+
+
 
 
 
