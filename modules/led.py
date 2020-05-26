2dbb8a1c3bb40f81598326d597d22c84794ac5a6
Joonas Erkheikki <erkheikki@hotmail.com>
2020-05-27 01:09:37 +0300

    led test

--- modules/led.py
+++ modules/led.py
@@ -1,29 +1,31 @@
 #!/usr/bin/env python3
 
-# import board
-# import neopixel
+import board
+import neopixel
 
 
 # Handle led brightness & color change.
 class LedControl:
     # LED strip configuration:
-    LED_COUNT = 20  # Number of LED pixels.
-    # LED_PIN = board.D21  # GPIO pin
+    LED_COUNT = 22  # Number of LED pixels.
+    LED_PIN = board.D21  # GPIO pin
     LED_BRIGHTNESS = 0.1  # LED brightness
     LED_MIN_BRIGHTNESS = 0.0
     LED_MAX_BRIGHTNESS = 0.9
-    # LED_ORDER = neopixel.GRBW  # order of LED colours. May also be RGB, GRBW, or RGBW
-    strip = ''
+    LED_ORDER = neopixel.GRBW  # order of LED colours. May also be RGB, GRBW, or RGBW
 
     def __init__(self):
-        self.strip = ''
-        # self.strip = neopixel.NeoPixel(
-        #    self.LED_PIN,
-        #    self.LED_COUNT,
-        #    brightness=self.LED_BRIGHTNESS,
-        #    auto_write=False,
-        #    pixel_order=self.LED_ORDER
-        # )
+        self.strip = neopixel.NeoPixel(
+            self.LED_PIN,
+            self.LED_COUNT,
+            brightness=self.LED_BRIGHTNESS,
+            auto_write=False,
+            pixel_order=self.LED_ORDER
+        )
 
     def set_brightness(self, val):
         self.LED_BRIGHTNESS = val
+
+    def change_color(self, val):
+        self.strip.fill(val)
+        self.strip.show()
